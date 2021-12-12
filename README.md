dc00de6ee8da3aee02cfd754f3b67ede89082bfa
Daniela Estrada <daniela.es.ib@gmail.com>
2021-12-11 20:49:18 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NodeJs---chat
+# nodeJs-chat
---
+++ client/src/components/home/Home.js
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { UserContext } from '../../UserContext'
+
+const Home = () => {
+  const { user, setUser } = useContext(UserContext)
+  const setAsJohn = () => {
+    const john = {
+      name: 'John',
+      email: 'john@email.com',
+      password: '123',
+      id: '123',
+    }
+    setUser(john)
+  }
+  const setAsTom = () => {
+    const tom = {
+      name: 'Tom',
+      email: 'john@email.com',
+      password: '456',
+      id: '456',
+    }
+    setUser(tom)
+  }
+  return (
+    <div>
+      <h1>Home {JSON.stringify(user)} </h1>
+      <button onClick={setAsJohn}>set as John</button>
+      <button onClick={setAsTom}>set as John</button>
+      <Link to='/chat'>
+        <button>Go to chat</button>
+      </Link>
+    </div>
+  )
+}
+
+export default Home
